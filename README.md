93ff90a27f3fd4e7ee1af64e75529e42e6488401
Coolguy38 <cadey.matignas@gmail.com>
2026-09-17 12:05:52 +0800

    Added .gitignore and the source code

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Python Terminal
-This is a terminal shell i made with python
+
+## This is a terminal shell I made with python
+
+It uses cmd under the hood to run terminal commands, but you can configure it to use a different shell.
+
+It has:
+
+- Cmd commands;
+- A custom python REPL;
+- And custom commands
---
+++ Terminal.py
@@ -0,0 +1,64 @@
+import os
+import subprocess
+import sys
+from time import sleep
+
+print("Welcome to the python terminal!")
+
+while True:
+	try:
+		prompt = f"PT -> {os.getcwd()}> "
+		cmd = input(prompt).strip()
+		commands = [c.strip() for c in cmd.split("&&")]
+
+		for command in commands:
+			if command.lower() == "exit":
+				print("Goodbye!")
+				sleep(0.5)
+				sys.exit()
+			elif command.lower() == "Coolguy38 is sigma":
+				print("Yes, im sigma B)")
+			elif command == "":
+				print("Empty command")
+			elif command.lower() == "python":
+				print(sys.version)
+				while True:
+					pyi = input(">")
+					if pyi.lower() == "exit" or pyi.lower() == "exit()":
+						break
+					elif pyi.lower() == "cls" or pyi.lower() == "clear":
+						subprocess.run("cls", shell=True)
+					else:
+						try:
+							exec(pyi)
+						except IndentationError:
+							blockly = [pyi]
+							while True:
+								extra = input("->")
+								if extra == "":
+									break
+								blockly.append(extra)
+							exec("\n".join(blockly))
+						except Exception as e:
+							print(f"{type(e)} happened with message {e}.")
+			elif command.lower() == "cd":
+				print(os.getcwd())
+
+			elif command.lower().startswith("cd "):
+				path = command[3:].strip()
+
+				try:
+					os.chdir(path)
+
+				except Exception as e:
+					print(f"{type(e)} happened with message {e}.")
+			else:
+				try:
+					subprocess.run(command, shell=True, stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr)
+
+				except Exception as e:
+					print(f"Error {e} with type {type(e)}")
+	except KeyboardInterrupt:
+		print("Exiting...")
+		sleep(0.5)
+		sys.exit()
